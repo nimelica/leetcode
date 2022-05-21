d83f8f2515034aba2ccd218f429432d589153ee2
Nimet Ozkan <76903207+nimelica@users.noreply.github.com>
2022-05-21 02:19:01 -0500

    Create two_pointers.cpp

---
+++ two_pointers.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        //(val1, index1)
+        //(val2, index2)
+        //index1 != index2
+        unordered_map<int, int>pairs;
+        for(int i = 0; i < nums.size(); ++i){
+            pairs[nums[i]] = i;
+        }
+        for(int i = 0; i < nums.size(); ++i){
+            int rem = target - nums[i];
+            if((pairs.find(rem) != pairs.end()) and (pairs[rem]) != i){
+                return {i, pairs[rem]};
+            }
+        }
+        return {-1, -1};
+    }
+     // two sum with a sorted list and the first index is 1 not 0
+      vector<int> twoSum2(vector<int>& numbers, int target) {
+        //sorted == binary search
+        int left = 0, right = numbers.size()-1;
+        while(left < right){
+            int sum = numbers[left] + numbers[right];
+            if(sum == target){
+                return {left+1, right+1};
+            }
+            else if(sum > target){
+                --right;
+            }
+            else{
+                ++left;
+            }
+        }
+        return {-1, -1};
+    }
+    
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>>ans;
+        if(nums.size() < 2){
+            return ans;
+        }
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i < nums.size(); ++i){
+            //handle duplicates
+            if(i > 0 and nums[i] == nums[i - 1])
+                continue;
+            int left = i + 1;
+            int right = nums.size() - 1;
+            int x = nums[i];
+            
+            while(left < right){
+                int sum = x + nums[left] + nums[right];
+                
+                if(sum < 0){
+                    left++;
+                }
+                else if(sum > 0){
+                    right--;
+                }
+                else{
+                    ans.push_back(vector<int>{x, nums[left], nums[right]});
+                    //handle duplications
+                    while(left < right and nums[left] == nums[left + 1]) 
+                        left++;
+                    while(left < right and nums[right] == nums[right - 1])
+                        right--;
+                    left++; right--;
+                }
+            }
+        }
+        return ans;
+    }
+    
+};
