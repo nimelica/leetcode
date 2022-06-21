3f1c192e3e3e1389602e649fa8ab4bca8efba99c
Nimet Ozkan <76903207+nimelica@users.noreply.github.com>
2022-06-20 23:03:20 -0500

    added valid parantheses

--- stack.cpp
+++ stack.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+using namespace std;
 
+ bool isValidParantheses(string s) {
+    stack<char> temp;
+    map<char, char> m = { {']','['},{')','('},{'}','{'} };
+     
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == '[' || s[i] == '(' || s[i] == '{')
+            temp.push(s[i]);
+        else if (s[i] == ']' || s[i] == ')' || s[i] == '}')
+        {
+            if (!temp.empty() and temp.top() == m[s[i]]) temp.pop();
+            else return false;
+        }
+    }
+    return temp.empty();
+ }
