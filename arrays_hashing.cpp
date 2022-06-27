5f53f34efcd3b2a81ef96e10c14237659d3cc7c5
Nimet Ozkan <76903207+nimelica@users.noreply.github.com>
2022-06-27 01:16:06 -0500

    binary search

--- arrays_hashing.cpp
+++ arrays_hashing.cpp
@@ -4,3 +4,17 @@
 #include <map>
 #include <set>
 using namespace std;
+
+class Solution {
+public:
+    int binary_search(vector<int>& nums, int target) {
+        int left = 0, right = nums.size()-1;
+        while(left<=right){
+            int midpoint = left+(right-left)/2;
+            if(nums[midpoint] == target) return midpoint;
+            else if(nums[midpoint] < target) left = midpoint + 1;
+            else right = midpoint - 1;
+        }
+        return -1;
+    }
+};
