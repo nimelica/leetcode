62c4bd380deb1d9300357277ee67f26197e1dd85
Nimet Ozkan <76903207+nimelica@users.noreply.github.com>
2022-07-19 01:29:05 -0500

    merge two sorted lists

--- linked_list.cpp
+++ linked_list.cpp
@@ -32,4 +32,33 @@ public:
         }
         return prev;
     }
+      
+    // merge two-sorted lists
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(!list1 and !list2) 
+            return nullptr;
+        else if(!list1 or !list2){
+            return !list1 ? list2 : list1;
+        }
+        
+        ListNode prev(-1);
+        ListNode* head = &prev;
+        
+        while(list1 != nullptr and list2 != nullptr){
+            if(list1->val < list2->val){
+                head->next = list1;
+                list1 = list1->next;
+            }
+            else{
+                head->next = list2;
+                list2 = list2->next;
+            }
+            head = head->next;
+        }
+        if(list1 or list2){
+            if(list1) head->next = list1;
+            else head->next = list2;
+        }
+        return prev.next;
+    }
 };
